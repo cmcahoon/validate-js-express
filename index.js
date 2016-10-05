9183bc0af9a602864145121629249a8064409f73
Charlie <charlie@airmap.com>
2016-10-04 17:08:43 -0700

    feat: basic express middleware

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 'use strict'
 
-module.exports = {}
+const _ = require('lodash')
+let validate = require('validate.js').validate
+
+
+function expressValidate(schema) {
+    // NOTE: This particular guard (isPlainObject) could potentially be too
+    // strict. What we are looking for is an object with keys, not an array,
+    // function, or any of the other types JS labels as objects. This check will
+    // look for an object that has `prototype === null`.
+    if (!_.isPlainObject(schema))
+        throw new Error('validation schema must be an object')
+
+    if (_.isEmpty(schema))
+        throw new Error('validation schema cannot be empty/undefined')
+
+    // NOTE: This guard could be seen as too strict and easily removed without
+    // damaging the integrity of the subsequent code paths. The reasons I put
+    // this check in place is because the most common issue with passing objects
+    // as parameters is small typos. With this check, if someone calls it with
+    // 'paths' instead of 'path' they will get an error. Without the check
+    // the code below will ignore 'paths' and they will not know validation
+    // was being skipped.
+    if (!_(schema).omit(['path', 'query', 'body']).isEmpty())
+        throw new Error('validation schema had unsupported keys')
+
+    // return the middleware function
+    return (req, res, next) => {
+        let errors = _({})
+            .merge(validate(req.params, schema.path))
+            .merge(validate(req.query, schema.query))
+            .merge(validate(req.body, schema.body))
+            .value()
+
+        // TODO: create ability to provide error formatter
+        _.isEmpty(errors) ? next() : res.status(400).json(errors)
+    }
+
+}
+
+module.exports = {
+    middleware: {
+        validate: expressValidate
+    }
+}
---
+++ test/unit/index.js
@@ -0,0 +1,72 @@
+'use strict'
+
+let chai = require('chai'),
+    expect = chai.expect
+let chaiAsPromised = require('chai-as-promised')
+let sinon = require('sinon')
+
+chai.use(chaiAsPromised)
+
+
+describe('express middleware', function(){
+
+    let validator = require('../../index.js')
+
+    it('should throw an error if the schema is undefined', function() {
+        let fn = () => validator.middleware.validate()
+        expect(fn).to.throw(/must be an object/)
+    })
+
+    it('should throw an error if the schema is empty', function() {
+        let fn = () => validator.middleware.validate({})
+        expect(fn).to.throw(/empty\/undefined/)
+    })
+
+    it('should throw an error if the schema is not an object', function() {
+        let fnArray = () => validator.middleware.validate([])
+        let fnNumber = () => validator.middleware.validate(42)
+
+        expect(fnArray).to.throw(/must be an object/)
+        expect(fnNumber).to.throw(/must be an object/)
+    })
+
+    it('should throw an error if the schema has unsupported keys', function() {
+        let fn = () => validator.middleware.validate({
+            foo: {
+                bar: { presence: true }
+            }
+        })
+
+        expect(fn).to.throw(/unsupported keys/)
+    })
+
+    // TODO: expand this test
+    it('should return validate.js errors', function() {
+        const schema = {
+            path: {
+                id: {
+                    presence: true,
+                    numericality: true
+                }
+            }
+        }
+
+        let req = {
+            params: {
+                id: "string"
+            }
+        }
+
+        let res = {}
+        res.json = sinon.spy()
+        res.status = sinon.stub().returns(res)
+
+        // generate the middleware function
+        let fn = validator.middleware.validate(schema)
+        fn(req, res, undefined)
+
+        expect(res.status.calledOnce).to.be.true
+        expect(res.status.args[0]).to.eql([ 400 ])
+        expect(res.json.calledOnce).to.be.true
+    })
+})
